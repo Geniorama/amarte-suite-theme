195101ed92c4620912598fb5ebeb9ae2338845d0
Darwin Roa <darwinmichaelroamora@gmail.com>
2024-06-25 18:24:27 -0500

    feat(shortcode price): Agrega attributo settings para indicar cual es el id del post de configuraciones generales del hotel

--- shortcodes/price/ha__price.php
+++ shortcodes/price/ha__price.php
@@ -2,7 +2,7 @@
 
 /**
  * Shortcode para mostrar el precio del plan en función del tipo de plan y del atributo que corresponde al bloque de hora
- * shortcode de referencia [ha_price_plan bloque_horas='costo_4horas']
+ * shortcode de referencia [ha_price_plan bloque_horas='costo_4horas' settings=7766]
  */
 if (!function_exists('ha_price_plan_func')) {
   add_shortcode('ha_price_plan', 'ha_price_plan_func');
@@ -15,11 +15,12 @@ if (!function_exists('ha_price_plan_func')) {
     $attributes = shortcode_atts(
       array(
         'bloque_horas'  => 'costo_4horas',
+        'settings'      => 7766 //ID del post de configuraciones generales.
       ),
       $atts
     );
     $planType = get_field('tipo_de_plan');
-    $planes = get_field('configuraciones_planes', 7766)['planes'];
+    $planes = get_field('configuraciones_planes', $attributes['settings'])['planes'];
     foreach ($planes as $plan) {
       if ($plan['tipo_de_plan'] === $planType) {
         return $plan[$attributes['bloque_horas']];
